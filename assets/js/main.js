438ebe92103a35975fae055e840ff28aec0f648a
Camila Fabbroni <camila.fabbroni1985@gmail.com>
2023-10-16 20:38:15 -0300

    primer commit

---
+++ assets/js/main.js
@@ -0,0 +1,61 @@
+window.onload = function() {
+  // Arrays para las diferentes partes de la excusa
+  let quien = ["Mi perro", "Mi hermana", "El gato de mi vecino", "Un pájaro"];
+  let accion = ["se comió", "mojó", "aplastó", "quemó"];
+  let que = ["mi tarea", "las llaves", "el auto"];
+  let cuando = [
+    "antes de la clase",
+    "cuando iba saliendo",
+    "cuando terminé",
+    "durante mi almuerzo",
+    "mientras me duchaba"
+  ]; 
+
+  // Función para generar una excusa aleatoria
+  function generarExcusa() {
+    let quienAleatorio = quien[Math.floor(Math.random() * quien.length)];
+    let accionAleatoria = accion[Math.floor(Math.random() * accion.length)];
+    let queAleatoria = que[Math.floor(Math.random() * que.length)];
+    let cuandoAleatoria = cuando[Math.floor(Math.random() * cuando.length)];
+
+    return `${quienAleatorio} ${accionAleatoria} ${queAleatoria} ${cuandoAleatoria}`;
+  }
+
+  // Agregar un evento al botón (se hace click en el botón y este genera una excusa random)
+  document.getElementById("generateExcuseButton").addEventListener("click", function() {
+    let excusa = generarExcusa();
+    document.getElementById("excuseContent").textContent = excusa;
+    document.getElementById("excuseModal").style.display = "block";
+});
+
+// Cierra el modal al hacer clic en la "x"
+document.getElementById("closeModal").addEventListener("click", function () {
+  document.getElementById("excuseModal").style.display = "none";
+});
+
+// Función para crear el efecto typewriter
+function typeWriter(textElement, text, delay, callback) {
+  let charIndex = 0;
+  const interval = setInterval(function () {
+    if (charIndex < text.length) {
+      textElement.innerHTML += text.charAt(charIndex);
+      charIndex++;
+    } else {
+      clearInterval(interval);
+      if (callback) {
+        callback(); // Llama a la función de callback después de terminar de escribir el texto
+      }
+    }
+  }, delay);
+}
+
+// Llamado a la función para aplicar el efecto typewriter al h1
+const header1 = document.getElementById("header1");
+typeWriter(header1, "Generador de Excusas", 100, function () {
+  // Llamado a la función para aplicar el efecto typewriter al h2 después de que h1 haya terminado
+  const header2 = document.getElementById("header2");
+  typeWriter(header2, "Presione el botón para generar una excusa aleatoria:", 100);
+});
+
+console.log("Hola Rigo desde la consola!");
+};
